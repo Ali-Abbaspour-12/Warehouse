96b5efd3988ef21f54fd13de25ec81833bdf667a
Ali-Abbaspour-12 <allah.aliabbaspour@gmail.com>
2025-11-19 04:31:12 -0800

    new changes

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_login import LoginManager, current_user
 from config import Config
 from extensions import db
 from models import User
+from waitress import serve
+
 
 
 from routes import (
@@ -56,3 +58,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", debug=True)
+    #serve(app, host="0.0.0.0", port=5000)
